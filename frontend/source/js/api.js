a83e8f19dbc392d66d4e7dd43641127cf17037ed
永野 朗夫 <nagano@me.com>
2021-07-01 16:03:12 +0900

    remove extra file

--- frontend/source/js/api.js
+++
@@ -1,3 +0,0 @@
-export const PROPERTIES = 'https://raw.githubusercontent.com/dbcls/togosite/develop/config/togosite-human/properties.json';
-export const TEMPLATES = 'https://raw.githubusercontent.com/dbcls/togosite/develop/config/togosite-human/templates.json';
-export const AGGREGATE = 'https://raw.githubusercontent.com/dbcls/togosite/develop/config/togosite-human/aggregate.json';
